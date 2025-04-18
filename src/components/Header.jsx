4ac0b70a45e67a29a33ed5027e36af9432ce5aba
Admin <3erkalome4tbi@gmail.com>
2025-04-18 12:48:52 +0700

    add_new

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?semt=ais_hybrid&w=740'>
+            </img>
+        </header>
+    );
+
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://gratisography.com/wp-content/uploads/2025/01/gratisography-dog-vacation-800x525.jpg'>
+                </img>
+            </div>
+            <div>
+                ava+desc
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
+        </div>
+    );
+
+}
+
+export default Content;
