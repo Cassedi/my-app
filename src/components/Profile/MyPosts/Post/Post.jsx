bbd1cf48b68135ef6cdd9277b15e5e0b4e61d019
Admin <3erkalome4tbi@gmail.com>
2025-04-21 15:17:21 +0700

    123

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfCBQAq4fDkjizcfZGBW_6ir3gL4Kd8b_3fA&s"></img>
-            post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span>like</span> {props.likesCount}
             </div>
 
         </div>
     );
-
+ 
 }
 
 export default Post;
